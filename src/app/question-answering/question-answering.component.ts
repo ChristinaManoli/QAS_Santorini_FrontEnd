afdeb9283e2982f5a950cf18a51b6762cc3fb687
ChristinaManoli <dai19181@uom.edu.gr>
2023-07-02 22:52:55 +0300

    Visualization changes

--- src/app/question-answering/question-answering.component.ts
+++ src/app/question-answering/question-answering.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { QaService } from '../services/qa.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import NeoVis from 'neovis.js/dist/neovis.js';
 
 
 export interface Message {
   type: string;
-  message: string;
+  message: string | HTMLElement;
 }
 
 @Component({
@@ -13,15 +14,17 @@ export interface Message {
   templateUrl: './question-answering.component.html',
   styleUrls: ['./question-answering.component.scss']
 })
-export class QuestionAnsweringComponent {
+export class QuestionAnsweringComponent implements OnInit{
   searchTerm: any;
   messages: Message[] = [];
   createForm: any ;
   question: any;
   response : any;
+  divElement!: HTMLDivElement;
+  private neoVis: any;
 
-
-  constructor(private service: QaService, private fb: FormBuilder) {}
+  constructor(private service: QaService, private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
     this.createForm = this.fb.group({
@@ -32,38 +35,58 @@ export class QuestionAnsweringComponent {
       type: 'system',
       message: 'Hi, I am your travel support agent for Santorini. How can I help you?',
     });
+
+    this.divElement = document.createElement("div");
+    this.divElement.id = "viz";
   }
 
   sendMessage(): void {
     this.question = this.createForm.get('question').value;
-
     this.messages.push({
       type: 'client',
       message: this.question,
     });
     
-    console.log(this.question);
-
     this.service.Question_Answering_System(this.question).subscribe({
       next: (res) => 
       {
         this.response = res
-        console.log(this.response);
         this.messages.push({
           type: 'system',
-          message: this.response.result1,
+          message: this.divElement,
         });
+        console.log(this.response.result1);
+
+        this.draw();
+
         this.messages.push({
           type: 'system',
           message: "Would you like to know more about: " + this.response.result2,
         });
       }
-      
     });
-    
-
-    
     this.createForm.reset();
-
   }
+
+  draw() {
+    console.log("hiiii");
+    var config = {
+      containerId: "viz",
+      neo4j: {
+        serverUrl: "neo4j://609579ea.databases.neo4j.io",
+        serverUser: "neo4j",
+        serverPassword: "santorinikg2023",
+      },
+      labels: {
+        
+      },
+      relationships: {
+      },
+      initialCypher: 'MATCH (start{ns0__name:"Santorini"})-[r:ns0__containsPlace]->(end:ns1__Village) RETURN *'
+    }
+
+    this.neoVis = new NeoVis(config);
+    this.neoVis.render();
+  }
+
 }
